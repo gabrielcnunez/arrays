372f6b2226257d9fae247485eca75663bb464fda
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-07-10 14:19:54 -0700

    Add methods with explanations

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ turing_students = ["April", "Bobby", "Candace", "Derrick"]
 student_ages = [23, 18, 51, 39]
 current_gpa = [3.81, 2.79, 3.23, 3.97]
 deposit_paid = [false, true, true, false]
+# Method "pop" will remove the last element in an array
+turing_students.Pop
+# Method "push" will push given object(s) onto the end of an array
+student_ages.push(36, 40)
+# Method "shift" removes first element from array
+current_gpa.shift
+# Method "unshift" puts element(s) in front of array, moving other element(s)
+deposit_paid.unshift(true, false)
